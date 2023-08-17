114c3b1480e8b8d838a20ab674930035d713d4f6
To <anhthichieu@gmail.com>
2023-08-17 21:15:19 +0700

    init landing page

--- app.vue
+++ app.vue
@@ -1,6 +1,184 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  <div>TJOI</div>
+  <header class="bg-white">
+    <nav class="mx-auto flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <div class="flex">
+        <a href="/" class="logo -m-1.5 p-1.5 text-3xl font-bold">
+          <span>SubRelay</span>
+        </a>
+      </div>
 
-  <button class="rounded bg-sky-500 hover:bg-sky-700">Save changes</button>
+      <div class="flex">
+        <div v-for="(options, index) in navOptions" :key="index" class="flex items-center m-2 cursor-pointer">
+          <span>{{ options }} </span>
+          <ChevronDownIcon class="h-4 w-4 ml-2" />
+        </div>
+      </div>
+
+      <div class="flex">
+        <div class="mr-2 border rounded-lg px-4 py-2 border-solid border-zinc-700 cursor-pointer hover:opacity-80">
+          Login
+        </div>
+
+        <div
+          class="mr-2 border rounded-lg px-4 py-2 border-solid bg-black text-gray-50 cursor-pointer hover:opacity-80"
+        >
+          Sign up
+        </div>
+      </div>
+
+      <!-- Mobile toggle button -->
+      <div class="flex lg:hidden">
+        <button
+          type="button"
+          class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          @click="mobileMenuOpen = true"
+        >
+          <span class="sr-only">Open main menu</span>
+          <Bars3Icon class="h-6 w-6" aria-hidden="false" />
+        </button>
+      </div>
+    </nav>
+
+    <!-- Mobile -->
+    <client-only>
+      <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+        <div class="fixed inset-0 z-10" />
+        <DialogPanel
+          class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+        >
+          <div class="flex items-center justify-between">
+            <a href="#" class="-m-1.5 p-1.5">
+              <span class="sr-only">Your Company</span>
+              <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+            </a>
+            <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
+              <span class="sr-only">Close menu</span>
+              <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div class="mt-6 flow-root">
+            <div class="-my-6 divide-y divide-gray-500/10">
+              <div class="space-y-2 py-6">
+                <Disclosure as="div" class="-mx-3" v-slot="{ open }">
+                  <DisclosureButton
+                    class="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Product
+                    <ChevronDownIcon :class="[open ? 'rotate-180' : '', 'h-5 w-5 flex-none']" aria-hidden="true" />
+                  </DisclosureButton>
+                  <DisclosurePanel class="mt-2 space-y-2">
+                    <DisclosureButton
+                      v-for="item in [...products, ...callsToAction]"
+                      :key="item.name"
+                      as="a"
+                      :href="item.href"
+                      class="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                      >{{ item.name }}</DisclosureButton
+                    >
+                  </DisclosurePanel>
+                </Disclosure>
+                <a
+                  href="#"
+                  class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >Features</a
+                >
+                <a
+                  href="#"
+                  class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >Marketplace</a
+                >
+                <a
+                  href="#"
+                  class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Company
+                </a>
+              </div>
+              <div class="py-6">
+                <a
+                  href="#"
+                  class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >Log in</a
+                >
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </client-only>
+  </header>
 </template>
+
+<script setup>
+import {
+  Dialog,
+  DialogPanel,
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Popover,
+  PopoverButton,
+  PopoverGroup,
+  PopoverPanel,
+} from '@headlessui/vue';
+import {
+  ArrowPathIcon,
+  Bars3Icon,
+  ChartPieIcon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  SquaresPlusIcon,
+  XMarkIcon,
+} from '@heroicons/vue/24/outline';
+
+import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/vue/20/solid';
+
+const products = [
+  {
+    name: 'Analytics',
+    description: 'Get a better understanding of your traffic',
+    href: '#',
+    icon: ChartPieIcon,
+  },
+  {
+    name: 'Engagement',
+    description: 'Speak directly to your customers',
+    href: '#',
+    icon: CursorArrowRaysIcon,
+  },
+  {
+    name: 'Security',
+    description: 'Your customers’ data will be safe and secure',
+    href: '#',
+    icon: FingerPrintIcon,
+  },
+  {
+    name: 'Integrations',
+    description: 'Connect with third-party tools',
+    href: '#',
+    icon: SquaresPlusIcon,
+  },
+  {
+    name: 'Automations',
+    description: 'Build strategic funnels that will convert',
+    href: '#',
+    icon: ArrowPathIcon,
+  },
+];
+const callsToAction = [
+  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
+  { name: 'Contact sales', href: '#', icon: PhoneIcon },
+];
+
+const mobileMenuOpen = ref(false);
+
+const showMenu = ref(false);
+const navOptions = ref(['Products', 'Template', 'Integrations', 'Resources', 'Pricing', 'Enterprices', 'Support']);
+</script>
+
+<style>
+.logo {
+  font-family: 'Unbounded';
+  user-select: none;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
+
+  modules: ["@nuxtjs/tailwindcss"],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-})
+
+  tailwindcss: {
+    cssPath: "~/assets/css/tailwind.css",
+    configPath: "~~/tailwind.config",
+    exposeConfig: false,
+    exposeLevel: 2,
+    config: {},
+    injectPosition: "first",
+    viewer: true,
+  },
+});
