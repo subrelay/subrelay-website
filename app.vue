4da2e1ae4357c9880b47d52356bd869097ca9531
To <anhthichieu@gmail.com>
2023-08-17 11:07:21 +0700

    feat: init tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <div>TJOI</div>
+
+  <button class="rounded bg-sky-500 hover:bg-sky-700">Save changes</button>
 </template>
