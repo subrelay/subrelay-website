068802155c2db5426d99d04e1504b27338400ed7
To <anhthichieu@gmail.com>
2023-08-30 16:43:04 +0700

    feat: faq

--- app.vue
+++ app.vue
@@ -17,6 +17,8 @@
 
   <Web2Web3Connection />
 
+  <FAQ />
+
   <div class="mx-auto max-w-[1370px] text-center pt-24 px-20 flex flex-col gap-4">
     <h2 class="text-[64px]">Do it all with a form</h2>
     <p class="leading-[133%] text-2xl font-normal">Whatever your goal, start with a ready-made template.</p>
