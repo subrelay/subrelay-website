4ea1eb0077fc9ba296f3f0234583ff3f9f0c899b
To <anhthichieu@gmail.com>
2023-09-03 22:26:58 +0700

    reponsive

--- components/HomePageHero.vue
+++ components/HomePageHero.vue
@@ -1,15 +1,19 @@
 7
 <template>
   <section class="pt-[72px] lg:pt-[88px]">
-    <div class="px-8 md:px-[60px] md:py-16 py-16 gap-6 xl:gap-8 grid grid-cols-12 max-w-[1370px] mx-auto first-letter">
+    <div
+      class="px-8 md:px-[60px] lg:px-[80px] md:py-16 py-16 gap-x-6 xl:gap-x-8 grid grid-cols-12 max-w-[1370px] mx-auto"
+    >
       <div class="col-span-full lg:col-start-1 lg:col-end-8 aspect-[960/728] mt-14 flex items-center justify-center">
-        <nuxt-img src="/placeholder.png" alt="" class="h-4/5 w-auto rounded-xl" />
+        <nuxt-img src="/placeholder.png" alt="" class="h-4/5 w-auto rounded-3xl" />
       </div>
 
       <div
         class="row-start-1 row-end-auto col-span-full lg:col-[8/-1] flex flex-col item-center text-center lg:text-left lg:items-start justify-center"
       >
-        <div class="text-6xl leading-none font-medium">Effortlessly connect your application to blockchain</div>
+        <div class="text-5xl md:text-6xl leading-none font-medium">
+          Effortlessly connect your application to blockchain
+        </div>
 
         <div class="mt-6 text-xl xl:text-2xl">
           Create workflows that synchronize on-chain data with off-chain applications —
